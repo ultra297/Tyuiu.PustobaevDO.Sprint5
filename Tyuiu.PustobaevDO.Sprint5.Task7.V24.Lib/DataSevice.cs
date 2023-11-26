638e2fbe2433b62f1cb7cdb72de233cf7ceaa11d
Tyuiu.PustobaevDO@outlook.com <Tyuiu.PustobaevDO@outlook.com>
2023-11-26 16:08:23 +0500

    Task7

---
+++ Tyuiu.PustobaevDO.Sprint5.Task7.V24.Lib/DataSevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+
+namespace Tyuiu.PustobaevDO.Sprint5.Task7.V24.Lib
+{
+    public class DataSevice
+    {
+    }
+}
---
+++ Tyuiu.PustobaevDO.Sprint5.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.PustobaevDO.Sprint5.Task7.V24.Lib;
+
+namespace Tyuiu.PustobaevDO.Sprint5.Task7.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
